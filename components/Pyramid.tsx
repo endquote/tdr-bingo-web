5855c4842e033ab236bd2094b04c15c9345171ff
Josh Santangelo <josh@endquote.com>
2022-11-12 08:00:27 -0800

    restructure pyramid so hot reload works

--- components/Pyramid.tsx
+++ components/Pyramid.tsx
@@ -4,33 +4,7 @@ import { Coordinate } from "ol/coordinate";
 import TileLayer from "ol/layer/Tile";
 import "ol/ol.css";
 import Zoomify from "ol/source/Zoomify";
-import { MouseEvent, useEffect, useRef } from "react";
-const mapSize = { w: 83512, h: 115478 };
-
-const layer = new TileLayer({
-  source: new Zoomify({
-    tileSize: 256,
-    tilePixelRatio: 1,
-    url: `/zoomify/img/`,
-    size: [mapSize.w, mapSize.h],
-    crossOrigin: "anonymous",
-  }),
-});
-
-const extent = layer.getSource()?.getTileGrid()?.getExtent();
-
-const resolutions = layer.getSource()!.getTileGrid()!.getResolutions();
-
-// limit min zoom https://openlayers.org/en/latest/examples/min-zoom.html
-const ol = new Map({
-  layers: [layer],
-  view: new View({
-    resolutions,
-    extent,
-    constrainOnlyCenter: true,
-    enableRotation: false,
-  }),
-});
+import { MouseEvent, useEffect, useMemo, useRef, useState } from "react";
 
 type Props = {
   onMapClick?: (coord: Coordinate) => void;
@@ -39,30 +13,71 @@ type Props = {
 export const Pyramid = ({ onMapClick }: Props) => {
   const mapRef = useRef<HTMLDivElement>(null!);
 
+  const mapSize = useMemo(() => {
+    return { w: 83512, h: 115478 };
+  }, []);
+
+  const layer = useMemo(() => {
+    return new TileLayer({
+      source: new Zoomify({
+        tileSize: 256,
+        tilePixelRatio: 1,
+        url: `/zoomify/img/`,
+        size: [mapSize.w, mapSize.h],
+        crossOrigin: "anonymous",
+      }),
+    });
+  }, [mapSize]);
+
+  const extent = useMemo(() => {
+    return layer.getSource()?.getTileGrid()?.getExtent();
+  }, [layer]);
+
+  const resolutions = useMemo(() => {
+    return layer.getSource()!.getTileGrid()!.getResolutions();
+  }, [layer]);
+
+  const [ol, setOl] = useState<Map>();
+
   useEffect(() => {
+    console.log("map setup");
+
+    const ol = new Map({
+      layers: [layer],
+      view: new View({
+        resolutions,
+        extent,
+        constrainOnlyCenter: true,
+        enableRotation: false,
+      }),
+    });
+
+    setOl(ol);
+
     ol.setTarget(mapRef.current);
     ol.getView().fit(extent!);
-  }, [mapRef]);
+
+    const ref = mapRef.current;
+    return () => {
+      console.log("map cleanup");
+      setOl(undefined);
+      ref.innerHTML = "";
+    };
+  }, [extent, layer, mapRef, resolutions]);
 
   function onClick(e: MouseEvent<HTMLDivElement>) {
-    if (!onMapClick) {
+    if (!onMapClick || !ol) {
       return;
     }
-
     const px = ol.getEventPixel(e as unknown as UIEvent);
-
     if (!px) {
       return;
     }
-
     const coord = ol.getCoordinateFromPixel(px);
-
     if (!coord) {
       return;
     }
-
     coord[1] *= -1;
-
     onMapClick(coord);
   }
 
