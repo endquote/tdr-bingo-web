df77ae61cff84b14abe262bb3c57e1e216a89330
Josh Santangelo <josh@endquote.com>
2022-11-12 11:05:57 -0800

    better map event handling

--- components/Pyramid.tsx
+++ components/Pyramid.tsx
@@ -1,10 +1,11 @@
 // @refresh reset
-import { Map, View } from "ol";
+import { Map, MapBrowserEvent, View } from "ol";
 import { Coordinate } from "ol/coordinate";
 import TileLayer from "ol/layer/Tile";
+import { ObjectEvent } from "ol/Object";
 import "ol/ol.css";
 import Zoomify from "ol/source/Zoomify";
-import { MouseEvent, useEffect, useMemo, useRef, useState } from "react";
+import { useCallback, useEffect, useMemo, useRef } from "react";
 
 type Props = {
   onMapClick?: (coord: Coordinate) => void;
@@ -13,80 +14,94 @@ type Props = {
 export const Pyramid = ({ onMapClick }: Props) => {
   const mapRef = useRef<HTMLDivElement>(null!);
 
-  const mapSize = useMemo(() => {
-    return { w: 83512, h: 115478 };
-  }, []);
+  const mapSize = useMemo(() => ({ w: 83512, h: 115478 }), []);
 
-  const layer = useMemo(() => {
-    return new TileLayer({
-      source: new Zoomify({
-        tileSize: 256,
-        tilePixelRatio: 1,
-        url: `/zoomify/img/`,
-        size: [mapSize.w, mapSize.h],
-        crossOrigin: "anonymous",
+  const layer = useMemo(
+    () =>
+      new TileLayer({
+        source: new Zoomify({
+          tileSize: 256,
+          tilePixelRatio: 1,
+          url: `/zoomify/img/`,
+          size: [mapSize.w, mapSize.h],
+          crossOrigin: "anonymous",
+        }),
       }),
-    });
-  }, [mapSize]);
+    [mapSize]
+  );
 
-  const extent = useMemo(() => {
-    return layer.getSource()?.getTileGrid()?.getExtent();
-  }, [layer]);
+  const extent = useMemo(
+    () => layer.getSource()?.getTileGrid()?.getExtent()!,
+    [layer]
+  );
 
-  const resolutions = useMemo(() => {
-    return layer.getSource()!.getTileGrid()!.getResolutions();
-  }, [layer]);
+  const resolutions = useMemo(
+    () => layer.getSource()?.getTileGrid()?.getResolutions()!,
+    [layer]
+  );
 
-  const [ol, setOl] = useState<Map>();
-
-  useEffect(() => {
-    console.log("map setup");
-
-    const ol = new Map({
-      layers: [layer],
-      view: new View({
+  const view = useMemo(
+    () =>
+      new View({
         resolutions,
         extent,
         constrainOnlyCenter: true,
         enableRotation: false,
       }),
-    });
+    [extent, resolutions]
+  );
 
-    setOl(ol);
+  const onChange = useCallback((e: ObjectEvent) => {
+    const view = e.target as View;
+    const [width, height] = e.target.viewportSize_;
+    const center = view.getCenter();
+    const resolution = view.getResolution();
+    const zoom = view.getZoom();
+    console.log({ type: e.type, width, height, center, resolution, zoom });
 
+    const outOfBounds = false;
+    if (outOfBounds) {
+      if (e.type === "change:center") {
+        view.un("change:center", onChange);
+        view.setCenter(e.oldValue);
+        view.on("change:center", onChange);
+      }
+      if (e.type === "change:resolution") {
+        view.un("change:resolution", onChange);
+        view.setResolution(e.oldValue);
+        view.on("change:resolution", onChange);
+      }
+    }
+  }, []);
+
+  const onClick = useCallback(
+    (e: MapBrowserEvent<PointerEvent>) => {
+      onMapClick?.(e.coordinate);
+    },
+    [onMapClick]
+  );
+
+  useEffect(() => {
+    const ol = new Map({ layers: [layer], view });
     ol.setTarget(mapRef.current);
-    ol.getView().fit(extent!);
+    view.fit(extent);
+    view.on("change:center", onChange);
+    view.on("change:resolution", onChange);
+    ol.on("click", onClick);
 
     const ref = mapRef.current;
     return () => {
-      console.log("map cleanup");
-      setOl(undefined);
       ref.innerHTML = "";
+      view.un("change:center", onChange);
+      view.un("change:resolution", onChange);
+      ol.un("click", onClick);
     };
-  }, [extent, layer, mapRef, resolutions]);
-
-  function onClick(e: MouseEvent<HTMLDivElement>) {
-    if (!onMapClick || !ol) {
-      return;
-    }
-    const px = ol.getEventPixel(e as unknown as UIEvent);
-    if (!px) {
-      return;
-    }
-    const coord = ol.getCoordinateFromPixel(px);
-    if (!coord) {
-      return;
-    }
-    coord[1] *= -1;
-    onMapClick(coord);
-  }
+  }, [extent, layer, mapRef, onChange, onClick, resolutions, view]);
 
   return (
-    <>
-      <div onClick={onClick} style={{ display: "flex", flex: 1 }}>
-        <div ref={mapRef} style={{ display: "flex", flex: 1 }}></div>
-      </div>
-    </>
+    <div style={{ display: "flex", flex: 1 }}>
+      <div ref={mapRef} style={{ display: "flex", flex: 1 }}></div>
+    </div>
   );
 };
 
