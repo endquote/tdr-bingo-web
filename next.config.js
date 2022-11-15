b7c45362e15ee2ed8d5b2c3afe86985e9831b65f
Josh Santangelo <josh@endquote.com>
2022-11-14 16:04:03 -0800

    maybe solved the weird jotai issue

--- next.config.js
+++ next.config.js
@@ -7,9 +7,9 @@ const nextConfig = {
   experimental: {
     swcPlugins: [
       // https://jotai.org/docs/api/swc#swc-jotai-debug-label
-      // ["@swc-jotai/debug-label", {}],
+      ["@swc-jotai/debug-label", {}],
       // https://jotai.org/docs/api/swc#swc-jotai-react-refresh
-      // ["@swc-jotai/react-refresh", {}],
+      ["@swc-jotai/react-refresh", {}],
     ],
   },
 };
