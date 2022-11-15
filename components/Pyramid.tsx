03c7e2c5117bf154e054065a3508769bb1ddd794
Josh Santangelo <josh@endquote.com>
2022-11-14 16:04:17 -0800

    zoom to tile with room for overlay

--- components/Pyramid.tsx
+++ components/Pyramid.tsx
@@ -1,5 +1,6 @@
 import { Map, MapBrowserEvent, View } from "ol";
 import { Coordinate } from "ol/coordinate";
+import { easeOut } from "ol/easing";
 import TileLayer from "ol/layer/Tile";
 import "ol/ol.css";
 import { Size } from "ol/size";
@@ -23,38 +24,44 @@ export const Pyramid = ({ onMapClick }: Props) => {
   // tell listeners which grid cell was clicked
   const onClick = useCallback(
     (e: MapBrowserEvent<PointerEvent>) => {
-      const [x, y] = e.coordinate;
-      const [w, h] = mapSize;
+      const [clickX, clickY] = e.coordinate;
+      const [mapW, mapH] = mapSize;
 
-      if (x < 0 || x > w) {
+      if (clickX < 0 || clickX > mapW) {
         return;
       }
 
-      if (y > 0 || y < -h) {
+      if (clickY > 0 || clickY < -mapH) {
         return;
       }
 
-      const [tw, th] = tileSize;
-      const tx = Math.floor(x / tw) + 1;
-      const ty = Math.floor(Math.abs(y) / th) + 1;
+      const [tileW, tileH] = tileSize;
+      const tileX = Math.floor(clickX / tileW) + 1;
+      const tileY = Math.floor(Math.abs(clickY) / tileH) + 1;
 
-      onMapClick?.([tx, ty]);
+      onMapClick?.([tileX, tileY]);
 
       const map = e.target as Map;
-      const [mw, mh] = map.getSize() || [0, 0];
       const view = map.getView();
+      const [viewW, viewH] = map.getSize()!;
 
-      const minX = tx * tw;
-      const minY = -ty * th;
-      const maxX = minX + tw;
-      const maxY = minY + th;
+      // compute the bounds of the tile
+      let minX = tileX * tileW - tileW;
+      let minY = -tileY * tileH;
+      let maxX = minX + tileW;
+      let maxY = minY + tileH;
 
-      view.fit([minX, minY, maxX, maxY]);
-      view.centerOn(
-        [minX + tw / 2 - tw, minY - th / 2 + th],
-        [mw, mh],
-        [mw / 2, mh / 2]
-      );
+      // make room for the overlay
+      let padding = [0, viewW / 3, 0, 0];
+      if (viewH > viewW) {
+        padding = [0, 0, viewH / 3, 0];
+      }
+
+      view.fit([minX, minY, maxX, maxY], {
+        padding,
+        duration: 200,
+        easing: easeOut,
+      });
     },
     [mapSize, onMapClick, tileSize]
   );
@@ -67,7 +74,8 @@ export const Pyramid = ({ onMapClick }: Props) => {
       resolutions: grid.getResolutions(),
       extent: extent,
       enableRotation: false,
-      showFullExtent: true,
+      // showFullExtent: true,
+      constrainOnlyCenter: true,
     });
 
     const ol = new Map({ layers: [new TileLayer({ source })], view });
@@ -88,6 +96,6 @@ export const Pyramid = ({ onMapClick }: Props) => {
       <div ref={mapRef} style={{ display: "flex", flex: 1 }}></div>
     </div>
   );
-};;;
+};
 
 export default Pyramid;
