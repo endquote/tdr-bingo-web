d0248c1df0676136b95398c3c9286eabc850290f
Josh Santangelo <josh@endquote.com>
2022-11-18 07:30:35 -0800

    giving up on sizing with js

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,12 +4,19 @@ import { Analytics } from "../components/Analytics";
 import { SiteHead } from "../components/SiteHead";
 import "../styles/globals.css";
 import styles from "../styles/Home.module.css";
+import { useWindowSize } from "../util/useWindowSize";
 
 export default function App({ Component, pageProps }: AppProps) {
+  // filling the window cross browser and on mobile with just css is basically impossible
+  const size = useWindowSize();
+
   return (
     <Provider>
       <SiteHead />
-      <div className={styles.container}>
+      <div
+        className={styles.container}
+        style={{ width: size.width, height: size.height }}
+      >
         <main className={styles.main}>
           <Component {...pageProps} />
         </main>
---
+++ util/useWindowSize.ts
@@ -0,0 +1,38 @@
+import { useEffect, useState } from "react";
+
+// stolen from here
+// https://usehooks.com/useWindowSize/
+
+// this one doesn't work
+// https://www.npmjs.com/package/@react-hook/window-size
+
+export function useWindowSize() {
+  const [windowSize, setWindowSize] = useState<{
+    width: number | undefined;
+    height: number | undefined;
+  }>({
+    width: undefined,
+    height: undefined,
+  });
+
+  useEffect(() => {
+    function handleResize() {
+      setWindowSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    }
+
+    window.addEventListener("resize", handleResize);
+    window.addEventListener("orientationchange", handleResize);
+
+    handleResize();
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+      window.removeEventListener("orientationchange", handleResize);
+    };
+  }, []);
+
+  return windowSize;
+}
